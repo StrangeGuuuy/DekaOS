2459ec847051ce8b54a503a78551298fb8bfb72f
FriendlyPup <rockyzikofficial@gmail.com>
2022-03-02 22:41:03 +0700

    Изменение в файлах запуска

--- INTRO_SCREEN.py
+++ INTRO_SCREEN.py
@@ -1,7 +1,7 @@
 import pyglet
 from pyglet.window import key
 
-window = pyglet.window.Window(fullscreen = True)
+window = pyglet.window.Window(fullscreen=True)
 
 img=pyglet.resource.image('project_icon.png')
 window.set_icon(img)
@@ -9,18 +9,16 @@ batch = pyglet.graphics.Batch()
 label = pyglet.text.Label('PROJECT: SHELL',
                           font_name='Times New Roman',
                           font_size=40,
-                          x=window.width//2, y=window.height//2, batch = batch)
+                          x=window.width//2, y=window.height//2, batch=batch, anchor_x='center')
 label1 = pyglet.text.Label('Press z to start new game',
                           font_name='Times New Roman',
                           font_size=18,
-                          x=window.width//2, y=window.height//2 - 50, batch = batch)
+                          x=window.width//2, y=window.height//2 - 50, batch=batch, anchor_x='center')
 label2 = pyglet.text.Label('Press x to go to "insert savepoint code" place',
                           font_name='Times New Roman',
                           font_size=18,
-                          x=window.width//2, y=window.height//2 - 100, batch = batch)
-label.anchor_x = 'center'
-label1.anchor_x = 'center'
-label2.anchor_x = 'center'
+                          x=window.width//2, y=window.height//2 - 100, batch=batch, anchor_x='center')
+
 @window.event
 def on_key_press(symbol, modifiers):
     if symbol == key.Z:
@@ -29,8 +27,10 @@ def on_key_press(symbol, modifiers):
     elif symbol == key.X:
         window.close()
         import CODE_INSERT
+
 @window.event
 def on_draw():
     window.clear()
     batch.draw()
+
 pyglet.app.run()
--- PROJECT(Запустить с этой программы!).py
+++ PROJECT(Запустить с этой программы!).py
@@ -1,16 +1,18 @@
 import tkinter
 
-window = tkinter.Tk()
-window.wm_attributes('-fullscreen',True)
-def handle_keypress(event):
+def handle_keypress(event) -> None:
     window.destroy()
     import INTRO_SCREEN
 
-window.title('Introduction.')
-window['bg'] = 'black'
+
+window = tkinter.Tk(className='Introduction.')
+window.wm_attributes('-fullscreen',True)
+
+window.config(bg='black')
 window.iconphoto(False, tkinter.PhotoImage(file='project_icon.png'))
-frame = tkinter.Frame(master = window)
-label = tkinter.Label(master = frame, font = 'Arial 50', text = "Hello.\nThis is the game project.\nIt's really raw, so be careful.\nHave fun.\n(press Enter to continue)", background = 'black', foreground = 'white')
+frame = tkinter.Frame(master=window)
+label = tkinter.Label(master=frame, font='Arial 50', text="Hello.\nThis is the game project.\nIt's really raw, so be careful.\nHave fun.\n(press Enter to continue)",
+                    background='black', foreground='white')
 frame.pack()
 label.pack()
 window.bind('<Return>', handle_keypress)
